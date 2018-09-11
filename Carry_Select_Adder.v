a9bbd37c7272095e7d72d58474d364cf2f30ee13
Oded Yosef <odedyosef@gmail.com>
2018-09-11 18:38:39 +0300

    Update Carry_Select_Adder.v

--- Carry_Select_Adder.v
+++ Carry_Select_Adder.v
@@ -1,15 +1,19 @@
-module Carry_Select_Adder(x,y,Cin,Cout,SUM);
+module Carry_Select_Adder(x,y,Cin,Cout,SUM);  
+// --------------------parameter-------------------
 parameter N=1;
+// --------------------inputs----------------------
 input [N:0]x;
 input [N:0]y;
 input Cin;
+// --------------------outputs---------------------
 output Cout;
 output [N:0]SUM;
-
+// --------------------wires-----------------------
 wire [N:0]s_zero;
 wire [N:0]s_one;
 wire cin_zero;
 wire cin_one;
+  
 //adder//
 assign {cin_zero,s_zero}=x+y+1'b0;
 assign {cin_one,s_one}=x+y+1'b1;
@@ -17,3 +21,4 @@ assign {cin_one,s_one}=x+y+1'b1;
 assign Cout = (Cin)?cin_one:cin_zero;
 assign SUM = (Cin)?s_one:s_zero;
 endmodule 
+// --------------------end--------------------------
